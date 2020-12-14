f95b5df8b0eaf85b9409fb1dc3a6e90ac31fc0ac
Malligarjunan <smalligarjunan@yahoo.com>
2020-12-14 23:02:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 'use strict';
 
 const express = require('express');
+const url = require('url');
 
 const PORT = 8000;
 const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('DevOPS CI/CD Handson!!\n');
+  
+  var q = url.parse(req.url, true).query;
+  /*Return the year and month from the query object:*/
+  var txt = q.year + " " + q.month;
+ 
+  res.send('DevOPS CI/CD Handson!!\n' + txt);
 });
 
 app.listen(PORT, HOST);
